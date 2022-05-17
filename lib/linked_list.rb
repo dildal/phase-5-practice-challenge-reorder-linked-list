09880b42756714140b4523591b75170203f5a455
Michael Dally <mddally@gmail.com>
2022-05-17 16:00:50 -0400

    Solved

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -8,6 +8,31 @@ class LinkedList
   end
 
   def reorder_linked_list
-    # your code here
+    return unless self.head
+    return unless self.head.next_node
+    return unless self.head.next_node.next_node
+    current = self.head
+    even_linked_list = LinkedList.new(current.next_node)
+    even_current = even_linked_list.head
+    current.next_node = current.next_node.next_node
+    current = current.next_node
+    while( current.next_node )
+        node_to_remove = current.next_node
+        next_even = Node.new(node_to_remove.data)
+        if( node_to_remove.next_node )
+          current.next_node = node_to_remove.next_node
+          even_current.next_node = next_even;
+          even_current = next_even
+          current = current.next_node
+        else
+          current.next_node = node_to_remove.next_node
+          even_current.next_node = next_even;
+          even_current = next_even
+          current.next_node = nil
+        end
+    end
+    current.next_node = even_linked_list.head
+    current
   end
+  
 end
